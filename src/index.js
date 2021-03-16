a201e0359316734207bff78273ecde7fee9eb8e3
Meshileya seun <meshileyaseun@gmail.com>
2021-03-16 12:20:19 +0100

    handle error

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import logger from './api/logger';
 import app from './api/server';
 
 // handle all uncaught errors
-// process.on("uncaughtException", function(err) {
-//   logger.error(`uncaught error has been fired with Error: ${err}`);
-// });
+process.on("uncaughtException", function(err) {
+  logger.error(`uncaught error has been fired with Error: ${err}`);
+});
 
 const server = http.createServer(app);
 const port = process.env.PORT || 80;
